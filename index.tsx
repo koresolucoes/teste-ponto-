03e817145b1e57501627e63b7b1dbc492fe36021
KoreSolucoes <koresoluciones@outlook.com>
2025-11-07 20:57:38 -0300

    feat: Add Portuguese locale for Angular
    Integrates the Portuguese locale (`pt-BR`) into the Angular application. This ensures that date, number, and currency formatting, as well as translations, will adhere to Brazilian Portuguese standards. This is crucial for a better user experience for Portuguese-speaking users.

--- index.tsx
+++ index.tsx
@@ -3,16 +3,21 @@ import '@angular/compiler';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter, withHashLocation } from '@angular/router';
-import { provideZonelessChangeDetection } from '@angular/core';
+import { LOCALE_ID, provideZonelessChangeDetection } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { AppComponent } from './src/app.component';
 import { APP_ROUTES } from './src/app.routes';
+
+registerLocaleData(localePt);
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideZonelessChangeDetection(),
     provideHttpClient(),
     provideRouter(APP_ROUTES, withHashLocation()),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
   ],
 }).catch((err) => console.error(err));
 
